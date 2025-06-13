05352ba8c07ce343066c227b585d5ef6dcb9f47b
kamaz <kryvoboker@gmail.com>
2025-06-13 20:45:11 +0300

    update TrelloService.php

--- httpdocs/app/Services/Api/Trello/TrelloService.php
+++ httpdocs/app/Services/Api/Trello/TrelloService.php
@@ -1,15 +1,21 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Services\Api\Trello;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TrelloService
 {
     public function handleList(Request $request) : void
     {
-        dump($request->all());
+        Log::channel('stack')->info(
+            'Trello webhook list request received',
+            [
+                'request' => $request->all(),
+            ]
+        );
     }
 }
