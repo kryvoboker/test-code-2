249e09f67dc813dcfbac4c3b1f3d3c425dfec8ad
kamaz <kryvoboker@gmail.com>
2025-06-13 22:17:47 +0300

    update StartCommand.php

--- httpdocs/app/TelegramBot/Commands/StartCommand.php
+++ httpdocs/app/TelegramBot/Commands/StartCommand.php
@@ -7,7 +7,7 @@ namespace App\TelegramBot\Commands;
 use App\Actions\Api\Telegram\SendMessageAction;
 use App\Contrancts\Telegram\TelegramCommandInterface;
 use App\Enums\Telegram\TelegramCommandEnum;
-use App\TelegramBot\Keyboards\Replies\MainMenuKeyboard;
+//use App\TelegramBot\Keyboards\Replies\MainMenuKeyboard;
 use Exception;
 use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Exception\TelegramException;
@@ -35,7 +35,7 @@ class StartCommand implements TelegramCommandInterface
             'text'         => __('telegram/index.text_start_command_text', [
                 'user_name' => get_telegram_user_firstname($update)
             ]),
-            'reply_markup' => app(MainMenuKeyboard::class)->getKeyboard(),
+//            'reply_markup' => app(MainMenuKeyboard::class)->getKeyboard(),
             'parse_mode'   => 'HTML',
         ];
 
