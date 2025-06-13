d17a14babedc4a2b422a6a46a0294af4fd4897a4
kamaz <kryvoboker@gmail.com>
2025-06-13 18:05:50 +0300

    update routes and composer.json

--- httpdocs/routes/api.php
+++ httpdocs/routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+declare(strict_types = 1);
+
 use App\Http\Controllers\Api\Telegram\TelegramController;
+use App\Http\Controllers\Api\Trello\TrelloController;
 use App\Http\Middleware\Api\TelegramApiAuth;
+use App\Http\Middleware\Api\TrelloApiAuth;
 
 Route::post('/telegram', [TelegramController::class, 'handle'])
     ->middleware(TelegramApiAuth::class)
     ->name('telegram.handle');
+
+Route::get('/trello-webhook-list', [TrelloController::class, 'returnStatusOk'])
+    ->name('trello.webhook.list.return-status-ok');
+
+Route::post('/trello-webhook-list', [TrelloController::class, 'handleList'])
+    ->middleware(TrelloApiAuth::class)
+    ->name('trello.webhook.list.handle-list');
