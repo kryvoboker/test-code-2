7b9a3370010cbf39245e67c0f036f2ac37122a94
kamaz <kryvoboker@gmail.com>
2025-06-13 14:45:05 +0300

    update user migration

--- httpdocs/database/migrations/0001_01_01_000000_create_users_table.php
+++ httpdocs/database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
                 ->nullable();
             $table
                 ->string('email')
+                ->nullable(false)
                 ->unique();
             $table
                 ->timestamp('email_verified_at')
